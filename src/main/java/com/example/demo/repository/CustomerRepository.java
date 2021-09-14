64003747c8efaa64149493519c37e73c310f5937
Dedenea <deseatmsts@gmail.com>
2021-09-14 21:10:44 +0700

    native branch

--- src/main/java/com/example/demo/repository/CustomerRepository.java
+++ src/main/java/com/example/demo/repository/CustomerRepository.java
@@ -1,5 +1,28 @@
 package com.example.demo.repository;
 import com.example.demo.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer,Integer>{
+    @Query(value = "select * from customer", nativeQuery = true)
+    List<Customer> getAll();
+
+    @Transactional
+    @Modifying
+    @Query(value = "insert into customer(name,age) values(?1,?2)", nativeQuery = true)
+    void addCustomer(String name, int age);
+
+    @Transactional
+    @Modifying
+    @Query(value = "update customer set name = ?2 where id = ?1", nativeQuery = true)
+    void updateCustomer(Integer id, String name);
+
+    @Transactional
+    @Modifying
+    @Query(value = "delete from customer where id = ?1", nativeQuery = true)
+    void deleteCustomerById(Integer id);
 }
---
+++ src/test/java/com/example/demo/RepositoryTest.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+import com.example.demo.repository.CustomerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RepositoryTest {
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Test
+    public void getAll() {
+        System.out.println("getAll: " + customerRepository.getAll());
+    }
+    @Test
+    public void add() {
+        customerRepository.addCustomer("Vu",22);
+    }
+    @Test
+    public void update() {
+        customerRepository.updateCustomer(1, "Vu2");
+    }
+    @Test
+    public void delete() {
+        customerRepository.deleteCustomerById(1);
+    }
+}
