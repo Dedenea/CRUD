453b2e2e7b6b11db928fa1f5224c202c0e472b06
Dedenea <deseatmsts@gmail.com>
2021-09-14 20:55:23 +0700

    repo branch

--- src/main/java/com/example/demo/entity/Customer.java
+++ src/main/java/com/example/demo/entity/Customer.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Customer {
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY)
---
+++ src/test/java/com/example/demo/RepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import com.example.demo.entity.Customer;
+import com.example.demo.repository.CustomerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RepositoryTest {
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Test
+    public void getAll(){
+        System.out.println("getAll: " + customerRepository.findAll());
+    }
+    @Test
+    public void add() {
+        Customer customer = Customer.builder().name("Hien nek").build();
+        System.out.println("Them Customer: " + customerRepository.save(customer));
+    }
+    @Test
+    public void update() {
+        Customer customer = Customer.builder().id(1).name("Hien update").build();
+        System.out.println("Update Customer: " + customerRepository.save(customer));
+    }
+    @Test
+    public void delete() {
+        customerRepository.deleteById(1);
+    }
+}
